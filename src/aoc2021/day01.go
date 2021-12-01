562cafb2b041e952eb63463bbc322a99f947455f
Anton Hemlin <anton.hemlin@gmail.com>
2021-12-02 00:43:03 +0100

    Day 1 finishedgit add -ugit add -u

--- src/aoc2021/day01.go
+++ src/aoc2021/day01.go
@@ -4,6 +4,7 @@ import (
     "fmt"
     "strings"
     "aoc/libs/utils"
+    "strconv"
 );
 
 /**
@@ -43,29 +44,59 @@ func main() {
 const separator string = "\n";
 
 var part1_test_input = []string{
-    ``,
+    `199
+200
+208
+210
+200
+207
+240
+269
+260
+263`,
 };
 var part1_test_output = []string{
-    ``,
+    `7`,
 };
 func part1(input string) string {
-    var inputs = strings.Split(strings.Trim(input, separator), separator);
-
-    // ...
-
-    return "";
+    var inputs = strings.Split(strings.Trim(input, separator), separator)
+    var inc = 0;
+    var prev = 9999999999999
+    for i := range inputs {
+        var cur, _ = strconv.Atoi(inputs[i])
+        //fmt.Printf("%d\n", prev)
+        //fmt.Printf("%d\n", cur)
+        if (cur > prev) {
+            inc++
+        }
+        prev = cur
+    }
+    //fmt.Printf("%d\n", inc)
+    return strconv.Itoa(inc)
 }
 
-var part2_test_input = []string{
-    ``,
-};
+var part2_test_input = part1_test_input
+
 var part2_test_output = []string{
-    ``,
+    `5`,
 };
 func part2(input string) string {
-    // var inputs = strings.Split(strings.Trim(input, separator), separator);
-
-    // ...
-
-    return "";
+    var inputs = strings.Split(strings.Trim(input, separator), separator);
+    inputs = append([]string{"999999999","99999999"}, inputs...)
+    var inc = 0;
+    var prev = 9999999999999
+    for i := 0; i < len(inputs) - 2; i++  {
+        var a, _ = strconv.Atoi(inputs[i])
+        var b, _ = strconv.Atoi(inputs[i+1])
+        var c, _ = strconv.Atoi(inputs[i+2])
+        var cur = a+b+c
+        //fmt.Printf("%d\n", prev)
+        //fmt.Printf("%d\n", cur)
+        if (cur > prev) {
+            inc++
+        }
+        prev = cur
+    }
+    //fmt.Printf("%d\n", inc)
+    return strconv.Itoa(inc)
 }
